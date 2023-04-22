af424f38ff38efc27133d23068033776ba859bcc
dudicoco <51051860+dudicoco@users.noreply.github.com>
2023-04-22 19:18:42 +0300

    Create main.tf

---
+++ module/main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.0.0"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.39.0"
+    }
+  }
+}
